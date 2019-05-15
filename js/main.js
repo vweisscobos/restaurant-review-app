84fe14ea606730a0a6c078a6d546dddadf0ee7fa
Vinicius Weiss <v.weisscobos@gmail.com>
2019-05-15 16:13:12 -0300

    FIX
    remove restaurant li from focus list

--- js/main.js
+++ js/main.js
@@ -157,7 +157,6 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-  li.setAttribute('tabindex', '0');
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
